956e77647df3563743ace303392a4f47d629df2a
Adrián Antón <adrtler@gmail.com>
2022-03-01 19:49:54 +0100

    Escribiendo los métodos update y delete en el Controlador Backend API Rest

--- src/main/java/com/bolsaideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsaideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,4 +44,24 @@ public class ClienteRestController {
 //		cliente.setCreateAt(new Date());
 		return clienteService.save(cliente);
 	}
+
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente clienteActual = clienteService.findById(id);
+		if (cliente.getApellido() != null)
+			clienteActual.setApellido(cliente.getApellido());
+		if (cliente.getNombre() != null)
+			clienteActual.setNombre(cliente.getNombre());
+		if (cliente.getEmail() != null)
+			clienteActual.setEmail(cliente.getEmail());
+		return clienteService.save(clienteActual);
+	}
+
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);
+	}
+
 }
