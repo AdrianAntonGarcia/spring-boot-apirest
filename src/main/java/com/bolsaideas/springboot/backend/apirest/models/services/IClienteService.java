f800e752699a8b67e8f3cd7d6ef291bc73a152a5
Adrián Antón <adrtler@gmail.com>
2022-03-01 19:22:01 +0100

    Escribiendo los métodos del CRUD en la clase ClienteService del Backend

--- src/main/java/com/bolsaideas/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/bolsaideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -7,4 +7,11 @@ import com.bolsaideas.springboot.backend.apirest.models.entity.Cliente;
 public interface IClienteService {
 
 	public List<Cliente> findAll();
+
+	public Cliente findById(Long id);
+
+	public Cliente save(Cliente cliente);
+
+	public void delete(Long id);
+
 }
